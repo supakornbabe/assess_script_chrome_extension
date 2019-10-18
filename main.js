cdd3aadec0c2d5870639499829fc9a3b03b1219c
Supakorn Wongsawang <supakorn.wo@ku.th>
2019-10-18 12:05:39 +0700

    Fix: random score from 3 to 5 instead of 4

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 var elementCollection = new Array();
 var allElements = document.getElementsByTagName("*");
+
+function getRandomIntWithFloor(max,floor) {
+  return Math.floor(Math.random() * Math.floor(max)) + floor;
+}
+
 for (var i = 0; i < allElements.length; i++) {
   if (
     allElements[i].id[0] == "c" &&
-    allElements[i].id[allElements[i].id.length - 1] == "4"
+    allElements[i].id[allElements[i].id.length - 1] == getRandomIntWithFloor(3,3).toString()
   )
     document.getElementById(allElements[i].id).checked = true;
 }
